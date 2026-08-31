a1dc1e8b858b48aa9d8d6c1e9677a28f01650ec2
AlissonLacayo <alissonlacayo04@gmail.com>
2026-08-31 07:45:57 -0500

    Add files via upload

---
+++ LibroCalificaciones_2/LibroCalificaciones_2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroCalificaciones_2
+{
+    internal class MiLibroCalificaciones
+    {
+        public void MostrarMensaje(string nombreCurso)
+        {
+            Console.WriteLine("¡Bienvendidos al libro de calificaiones para: \n{0} !",nombreCurso);
+        }
+    }
+}
---
+++ LibroCalificaciones_2/LibroCalificaciones_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroCalificaciones_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MiLibroCalificaciones MyLibro = new MiLibroCalificaciones();
+
+            Console.WriteLine("Por favor ingrese el nombre del curso: ");
+            string nombreDelCurso = Console.ReadLine();
+
+            int numero;
+
+            while(nombreDelCurso == "" || int.TryParse(nombreDelCurso, out numero))
+            {
+                if (nombreDelCurso == "")
+                {
+                    Console.WriteLine("Error: El nombre del curso no puede estar vacio.");
+                }
+                else if (int.TryParse(nombreDelCurso, out numero))
+                {
+                    Console.WriteLine("Error: El nombre del curso no puede ser solo numero.");
+                }
+                Console.WriteLine("Por favor intente nuevamente.\n");
+                nombreDelCurso = Console.ReadLine() ;
+            }
+
+            Console.WriteLine();
+            MyLibro.MostrarMensaje(nombreDelCurso);
+        }
+    }
+}
