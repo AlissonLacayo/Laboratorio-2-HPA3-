429bb83b86c93072f993bfaf092514cb68f40fba
AlissonLacayo <alissonlacayo04@gmail.com>
2026-08-31 07:45:31 -0500

    Add files via upload

---
+++ LibroCalificaciones_1/LibroCalificaciones_1/LIbroCalificacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroCalificaciones_1
+{
+    public class LIbroCalificacion
+    {
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenidos al libro de calificaciones");
+        }
+            
+    }
+}
---
+++ LibroCalificaciones_1/LibroCalificaciones_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroCalificaciones_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LIbroCalificacion MyLibro = new LIbroCalificacion();
+            MyLibro.MostrarMensaje();
+        }
+    }
+}
